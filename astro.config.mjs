3c2c138c0604e4947c51e300dbcc8cc9e3ff0919
Tux3er <isma.tux3er@gmail.com>
2024-12-22 11:41:13 +0100

    Fixing Deploying Bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pymewebteam-reservation.netlify.app',
+  outDir: './dist',
   output: 'server',
   adapter: netlify({
     edgeMiddleware: true
